49b2b911acba43007b78d2b78a1df79c0bf7db6c
Dev-BilalMunir <>
2023-12-01 11:30:41 +0500

    dist folder

---
+++ dist/assets/HomeView-17f6f912.js
@@ -0,0 +1,1 @@
+import{o as e,c as o}from"./index-15f44520.js";const s={__name:"HomeView",setup(t){return console.log("HomeView.vue setup()"),(c,n)=>(e(),o("div",null,"Home"))}};export{s as default};
